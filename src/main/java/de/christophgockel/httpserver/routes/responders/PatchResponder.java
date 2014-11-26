cbe9eff09630c04c4dbb671a041627113921c24a
Christoph Gockel <github@christophgockel.de>
2014-11-26 13:38:27 +0000

    Add regular GET support to PatchResponder

--- src/main/java/de/christophgockel/httpserver/routes/responders/PatchResponder.java
+++ src/main/java/de/christophgockel/httpserver/routes/responders/PatchResponder.java
@@ -1,10 +1,13 @@
 package de.christophgockel.httpserver.routes.responders;
 
 import de.christophgockel.httpserver.RequestMethod;
-import de.christophgockel.httpserver.StatusCode;
 import de.christophgockel.httpserver.filesystem.FileSystem;
 import de.christophgockel.httpserver.http.Request;
 import de.christophgockel.httpserver.http.Response;
+
+import static de.christophgockel.httpserver.RequestMethod.GET;
+import static de.christophgockel.httpserver.RequestMethod.PATCH;
+import static de.christophgockel.httpserver.StatusCode.*;
 
 public class PatchResponder extends BaseResponder {
   private final FileSystem fileSystem;
@@ -15,18 +18,38 @@ public class PatchResponder extends BaseResponder {
 
   @Override
   protected boolean respondsTo(RequestMethod method, String path) {
-    return method == RequestMethod.PATCH && path.equals("/patch-content.txt");
+    return path.equals("/patch-content.txt")
+      && (method == PATCH
+       || method == GET);
   }
 
   @Override
   protected Response respond(Request request) {
+    if (isGetRequest(request)) {
+      return get(request);
+    } else {
+      return patch(request);
+    }
+  }
+
+  private Response get(Request request) {
+    DefaultResponder defaultResponder = new DefaultResponder(fileSystem);
+
+    return defaultResponder.respond(request);
+  }
+
+  private boolean isGetRequest(Request request) {
+    return request.getMethod() == GET;
+  }
+
+  private Response patch(Request request) {
     String requestedResource = request.getURI();
 
     if (fileSystem.isFile(requestedResource)) {
       String requestSHA1 = request.getHeaders().get("If-Match");
 
       if (requestSHA1 == null) {
-        return new Response(StatusCode.PRECONDITION_FAILED);
+        return new Response(PRECONDITION_FAILED);
       }
 
       String resourceSHA1 = fileSystem.getSHA1ForFile(requestedResource);
@@ -35,12 +58,12 @@ public class PatchResponder extends BaseResponder {
         String content = request.getBody();
         fileSystem.setFileContent(requestedResource, content.getBytes());
 
-        return new Response(StatusCode.NO_CONTENT);
+        return new Response(NO_CONTENT);
       } else {
-        return new Response(StatusCode.PRECONDITION_FAILED);
+        return new Response(PRECONDITION_FAILED);
       }
     } else {
-      return new Response(StatusCode.NOT_FOUND);
+      return new Response(NOT_FOUND);
     }
   }
 }
